3e907099fef5091590f61b28d13e04fcdcb771da
Tomasz Cichon <tomekcichon@tlen.pl>
2023-06-13 09:57:35 +0200

    add add class active function

--- js/script.js
+++ js/script.js
@@ -7,9 +7,10 @@
 
 
   const titleClickHandler = function(event){
+    const clickedElement = this;
     console.log('Link was clicked!');
   
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -19,9 +20,15 @@
   
     /* add class 'active' to the clicked link */
 
+    clickedElement.classList.add('active');
     
   
-    /* remove class 'active' from all articles */
+
+    console.log('clickedElement:', clickedElement);
+
+    
+  
+    /* [DONE] remove class 'active' from all articles */
 
     const activeArticles = document.querySelectorAll('.posts article.active');
     console.log (activeArticles);
