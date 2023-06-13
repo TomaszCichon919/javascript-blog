df7409fa55ca916fc09e372fce3d1de238fcc894
Tomasz Cichon <tomekcichon@tlen.pl>
2023-06-13 10:25:04 +0200

    add compleate artice secection function

--- js/script.js
+++ js/script.js
@@ -1,12 +1,7 @@
 'use strict';
 
-/*document.getElementById('test-button').addEventListener('click', function(){
-    const links = document.querySelectorAll('.titles a');
-    console.log('links:', links);
-  });*/
-
-
   const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
   
@@ -18,12 +13,9 @@
     activeLink.classList.remove('active');
     }
   
-    /* add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
 
     clickedElement.classList.add('active');
-    
-  
-
     console.log('clickedElement:', clickedElement);
 
     
@@ -37,11 +29,22 @@
     activeArticle.classList.remove('active');
     }
   
-    /* get 'href' attribute from the clicked link */
+    /* [DONE] get 'href' attribute from the clicked link */
+
+    const articleSelector = clickedElement.getAttribute("href");
+    console.log (articleSelector);
   
-    /* find the correct article using the selector (value of 'href' attribute) */
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
   
-    /* add class 'active' to the correct article */
+    /* [DONE] add class 'active' to the correct article */
+
+    targetArticle.classList.add('active');
+    console.log('clickedElement:', clickedElement);
+
+
   console.log (event);
   }
   
